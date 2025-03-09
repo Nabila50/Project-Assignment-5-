6f65e55d6bc4b54fddf8e659fdbc8869c94098b2
Nabila <nabila.nawshin50@gmail.com>
2025-03-09 23:57:36 +0100

    blog.html added

---
+++ script/discover.js
@@ -0,0 +1,8 @@
+document.getElementById('discover').addEventListener('click', function(){
+ 
+    window.location.href="blog.html";
+})
+document.getElementById('desk-btn').addEventListener('click', function(){
+ 
+    window.location.href="index.html";
+})
