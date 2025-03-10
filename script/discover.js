ec1508a16146469981ea5ca193a49edb0b8138aa
Nabila <nabila.nawshin50@gmail.com>
2025-03-10 03:00:38 +0100

    card update

--- script/discover.js
+++ script/discover.js
@@ -2,7 +2,3 @@ document.getElementById('discover').addEventListener('click', function(){
  
     window.location.href="blog.html";
 })
-document.getElementById('desk-btn').addEventListener('click', function(){
- 
-    window.location.href="index.html";
-})
